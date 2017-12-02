21b303f2b7b0b6ccb89a2be6b846f3c47677a1b5
Venurs <1411716329@qq.com>
2017-12-02 16:03:37 +0800

    完成简单分布式系统

---
+++ master.py
@@ -0,0 +1,60 @@
+"""
+分布式进程
+master
+"""
+
+
+from multiprocessing.managers import BaseManager
+import queue
+import random
+
+
+
+
+
+def result_fun(list_result):
+    for res in list_result:
+        print(res)
+
+
+#  设置任务队列和接收返回结果的队列
+task_queue = queue.Queue()
+result_queue = queue.Queue()
+
+
+def return_task_queue():
+    global task_queue
+    return task_queue
+
+
+def return_result_queue():
+    global result_queue
+    return result_queue
+
+
+if __name__ == '__main__':
+
+    #  将队列注册到网络BaseManager.register
+    BaseManager.register("get_task_queue", callable=return_task_queue)
+    BaseManager.register("get_result_queue", callable=return_result_queue)
+    #  绑定端口 BaseManager()
+    m = BaseManager(address=("10.0.151.206", 5000), authkey=b"1234")
+    #  启动队列
+    m.start()
+    #  获得网络访问对象
+    task = m.get_task_queue()
+    result = m.get_result_queue()
+    #  放任务到task_queue
+    list_result = []
+    task.put(list_result)
+    print("试图获取执行结果中。。。")
+    #  接收任务执行结果到result_queue
+    list_result = result.get(timeout=10)
+    result_fun(list_result)
+    print('Try get results...')
+    #  关闭
+    m.shutdown()
+
+    print("master执行完毕")
+
+
---
+++ worker.py
@@ -0,0 +1,40 @@
+"""
+分布式进程
+worker
+"""
+
+import time
+import random
+from multiprocessing.managers import BaseManager
+
+
+def task_fun(list1):
+    print("任务执行中。。。")
+    for i in range(1, 20):
+        num = random.randint(1, 10000)
+        list1.append(num)
+        print("将%d添加到链表末尾" % num)
+    return list1
+
+
+#  注册
+BaseManager.register("get_task_queue")
+BaseManager.register("get_result_queue")
+#  链接服务器，包括IP和端口，口令
+server_ip = "192.168.253.1"
+w = BaseManager(address=(server_ip, 5000), authkey=b"1234")
+w.connect()
+#  获取对象
+task = w.get_task_queue()
+result = w.get_result_queue()
+# task取任务
+list_task = task_fun(task.get(timeout=5))
+# result放入执行结果
+result.put(list_task)
+
+print("worker 执行完毕。。。")
+
+
+
+
+
